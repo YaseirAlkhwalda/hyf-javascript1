c05a6332e1eabf05fbbb9324c41979c31fbfcb06
YaseirAlkhwalda <43831623+YaseirAlkhwalda@users.noreply.github.com>
2018-12-11 02:54:18 +0100

    Update main.js

--- Week2/main.js
+++ Week2/main.js
@@ -1,1 +1,106 @@
+/*
+let  helloWorld = "Hello, World!",  // English
+     helloSpain = "Hola, Mundo!",  // Spanish
+     helloItalian = "Ciao, Mondo!";  // Italian
 
+     console.log(helloWorld, helloSpain, helloItalian);
+*/
+
+// console.log('I\'m awesome');
+
+//let x;
+
+//console.log("The value of my variable x will be : " ); // undefined
+//console.log("The value of my variable x will be: " + x);
+/*
+let  x = 27;
+
+console.log("The value of my variable x will be: " + x);
+*/
+
+/*
+let  y = "Yaseir Alkhwalda";
+
+console.log("My Name is: " + y);
+
+     y = "My Country";
+
+console.log("My Lovely Country is: " + y);
+*/
+/*
+let  z = 7.25;
+
+console.log(z);
+
+let  a = 7.25;
+
+console.log(Math.floor(a));
+
+var bigNumber = Math.max(a, z);
+
+console.log(bigNumber);
+*/
+
+/*
+ let  myAnimals = ["cats", "dogs", "horses"]
+
+ console.log("My favorite Animals are: " + myAnimals);
+
+ myAnimals.push("baby pig");
+
+ console.log("My favorite Animals are: " + myAnimals);
+*/
+ 
+/*
+ let  myString = "this is a test";
+
+ //console.log(myString);
+
+ console.log(myString.length);
+*/
+/*
+ let myName = "Yaseir Alkhwalda",
+   
+    myAge = 27,    
+
+    myHobbies = ["swim", "jump", "gaming"],
+
+    myEmails = {youtube: "YSR-KH", gmail: "yaseir"};
+
+    console.log("My name: " + myName);
+
+    console.log("My Age: " + myAge);
+
+    console.log("My Hobbies: " + myHobbies);
+
+    console.log("My E.Mails: " + myEmails.youtube + " " + myEmails.gmail);
+
+    console.log(typeof myName, typeof myAge, typeof myHobbies, typeof myEmails);
+
+    if (myName === myAge) {
+        console.log("SAME TYPE");
+    } else {
+        console.log("Not the same type"); 
+    }
+
+    if (myHobbies === myEmails) {
+        console.log("SAME TYPE");
+    } else {
+        console.log("Not the same type"); 
+    }
+*/
+/*
+let  x = 7;
+
+console.log(x = x % 3);
+
+*/
+
+/*
+let  myInfo = ["Yaseir Alkhwalda", 27]
+
+console.log(myInfo);
+
+console.log(6/0 === 10/0);
+
+*/
