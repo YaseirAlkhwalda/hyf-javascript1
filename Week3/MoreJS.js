d961111ebb3bfa96278252d555bcbdd9bb955080
YaseirAlkhwalda <43831623+YaseirAlkhwalda@users.noreply.github.com>
2018-12-19 16:27:14 +0100

    home-work

---
+++ Week3/MoreJS.js
@@ -0,0 +1,35 @@
+
+function sum() {
+  let a = 1,
+      b = 2,
+      c = 3;
+
+return a + b + c;
+}
+
+sum();
+
+function colorCar() {
+      let red = "redcar",
+          green = "greencar";
+  return green;
+}
+colorCar();
+
+
+if (3 === 3) {
+  console.log("yes");
+} else {
+  console.log("no");
+}
+
+console.log(3 === 3 ? true : false);
+
+var teachers = {};
+
+let x = [1, 2, 3];
+let y = [1, 2, 3];
+let z = y;
+
+let bar = 42;
+typeof typeof bar;
